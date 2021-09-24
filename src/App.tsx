9898dce545c30cace0dd41a27cda29acc01b34e5
hoapham <hoang.trung.hai.pham@zalando.de>
2021-09-25 00:34:08 +0200

    Make use of all components into main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,55 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import "./App.css";
+import AddVideoForm from "./component/AddVideoForm";
 import Homepage from "./component/Homepage";
+import SignUpOrSignInForm, { User } from "./component/SignupOrSignInForm";
+import { content } from "./database";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
+  const [videoList, setVideoList] = useState(content.videos);
+  const [userList, setUserList] = useState(content.users);
+  const [currentUser, setCurrentUser] = useState<User | undefined>(undefined);
+
+  const handleAddVideoFormChange = useCallback((url: string) => {
+    const newVideo = {
+      id: Math.random() * 10000000,
+      url,
+      uploader: "Suprised",
+    };
+    setVideoList((oldList) => [...oldList, newVideo]);
+  }, []);
+
+  const handleSignUpOrSignInChange = useCallback(
+    (user: User) => {
+      setCurrentUser(user);
+
+      if (userList.findIndex((u) => u.email === user.email) === -1)
+        setUserList((oldList) => [...oldList, user]);
+    },
+    [userList]
+  );
+
   return (
-    <div className="App">
-      <Homepage></Homepage>
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <SignUpOrSignInForm
+              userList={userList}
+              onChange={handleSignUpOrSignInChange}
+            ></SignUpOrSignInForm>
+
+            {currentUser && <Link to="/share">Share</Link>}
+
+            <Homepage videoList={videoList}></Homepage>
+          </Route>
+
+          <Route path="/share">
+            <AddVideoForm onChange={handleAddVideoFormChange}></AddVideoForm>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/database.ts
@@ -0,0 +1,21 @@
+export let content = {
+  videos: [
+    {
+      id: 1,
+      url: "https://www.youtube.com/watch?v=Ilui-mb3sT0",
+      uploader: "haipham@gmail.com",
+    },
+    {
+      id: 2,
+      url: "https://www.youtube.com/watch?v=HbHAMzv1bUs",
+      uploader: "haipham@gmail.com",
+    },
+  ],
+  users: [
+    {
+      id: 10000001,
+      email: "haipham@gmail.com",
+      password: "123",
+    },
+  ],
+};
